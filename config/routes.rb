40e2a8999ce5d6e8fe0542eee97eb2673a61e52f
Yalun Qin <allenchin1990@gmail.com>
2014-10-27 23:46:32 -0700

    Add validation on email and name

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+ 
+   root 'main_static_pages#home'
+   
    get 'help'    => 'main_static_pages#help'
    get 'about'   => 'main_static_pages#about'
    get 'contact' => 'main_static_pages#contact'
+
+   get 'signup'  => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'main_static_pages#home'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
