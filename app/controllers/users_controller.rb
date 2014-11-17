63f08cd5740c50aec95697335fbc015957e9e753
Chad Simmons <polpak@gmail.com>
2014-11-17 15:01:15 -0800

    Added more content to the user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+    @posted_quests = DeliveryQuest.where(quest_giver: @user)
+    @accepted_quests = DeliveryQuest.where(quester: @user)
   end
 
   def create
