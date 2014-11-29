e4cd84a04fd299473befcd1628eac9a70bbf51e9
Yalun Qin <allenchin1990@gmail.com>
2014-11-28 22:22:03 -0800

    Added create method to advanced_search controller

--- app/controllers/advanced_searches_controller.rb
+++ app/controllers/advanced_searches_controller.rb
@@ -4,4 +4,10 @@ class AdvancedSearchesController < ApplicationController
     @advanced_search = AdvancedSearch.new
     
   end
+
+  def create
+    @delivery_quests = DeliveryQuest.all.paginate(:page => params[:page], :per_page => 10)
+    redirect_to delivery_quests_path
+  end
+
 end
