1e0aa7567404fb1c36dd6ee42aa736745dd35c43
Yalun Qin <allenchin1990@gmail.com>
2014-12-14 12:45:02 -0800

    Added indices to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204050411) do
+ActiveRecord::Schema.define(version: 20141214204225) do
 
   create_table "quests", force: true do |t|
     t.string   "title"
@@ -36,5 +36,8 @@ ActiveRecord::Schema.define(version: 20141204050411) do
     t.integer  "ratings_count"
     t.integer  "score",           default: 100
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["id"], name: "index_users_on_id", unique: true
 
 end
